bfc089266fa3c26e0fb123aa27f7025a0399c60c
Andrew Towner <keytonic@gmail.com>
2025-02-02 20:54:02 -0500

    Commit

--- assets/script.js
+++ assets/script.js
@@ -17,4 +17,23 @@ window.onload = function ()
             //nav.classList.remove('sticky');
         }
     }
+
+    let andrew = document.getElementById("andrew");
+    andrew.addEventListener("mouseover", function() 
+    {
+        document.getElementById("logo").style.fill = "red";
+    });
+    andrew.addEventListener("mouseout", function() 
+    {
+        document.getElementById("logo").style.fill = "inherit";
+    });
+    let towner = document.getElementById("towner");
+    towner.addEventListener("mouseover", function() 
+    {
+        document.getElementById("logot").style.fill = "red";
+    });
+    towner.addEventListener("mouseout", function() 
+    {
+        document.getElementById("logot").style.fill = "transparent";
+    });
 }
