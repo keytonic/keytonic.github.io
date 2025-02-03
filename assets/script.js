36395e0e265621a7de725ff74a036bea3f5ac036
Andrew Towner <keytonic@gmail.com>
2025-02-02 22:24:21 -0500

    commit

--- assets/script.js
+++ assets/script.js
@@ -4,17 +4,17 @@ window.onload = function ()
     window.onscroll = function()
     {
         const header = document.getElementById("site-header");
-        //const nav = document.getElementById("site-nav");
+        const nav = document.getElementById("site-nav");
 
         if (window.scrollY > 0) 
         {
             header.classList.add('sticky');
-            //nav.classList.add('sticky');
+            nav.classList.add('sticky');
         } 
         else 
         {
             header.classList.remove('sticky');
-            //nav.classList.remove('sticky');
+            nav.classList.remove('sticky');
         }
     }
 
