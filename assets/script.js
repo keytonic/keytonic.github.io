2ed59a58fe342d473305478d49fa9530e646f57a
Andrew Towner <keytonic@gmail.com>
2025-02-14 14:07:28 -0500

    Commit

--- assets/script.js
+++ assets/script.js
@@ -79,4 +79,79 @@ window.onload = function ()
         a.style.fill = "inherit";
         t.style.fill = "transparent";
     });
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.from(forms).forEach(form => 
+        {
+            form.addEventListener('submit', handleSubmit, false)
+        })
+
+
+
+}
+
+function handleSubmit(event) 
+{
+    event.preventDefault();
+    
+    if (!event.target.checkValidity()) 
+    {
+        event.preventDefault();
+        event.stopPropagation();
+        //alert("bad");
+    }
+    else
+    {
+        //alert("good");
+        let name = document.getElementsByName("name")[0];
+        let phone = document.getElementsByName("phone")[0];
+        let email = document.getElementsByName("email")[0];
+        let message = document.getElementsByName("message")[0];
+
+        var http = new XMLHttpRequest();
+        
+        http.onreadystatechange = function () 
+        {
+            if (this.readyState == 4 && this.status == 200)
+            {
+                alert("email sent");
+            }
+        };
+
+        http.open('POST', 'https://formspree.io/f/myzknyzb', true);
+        http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        http.send(`name=${name}&phone=${phone}&email=${email}&message=${message}`);
+
+    }
+
+    event.target.classList.add('was-validated');
+
+
+    
+    
+
+    /*
+    let name = document.getElementsByName("fname");
+    let phone = document.getElementsByName("fname");
+    let email = document.getElementsByName("fname");
+    let message = document.getElementsByName("fname");**/
+/*
+    var http = new XMLHttpRequest();
+
+    http.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            setmyAlert({ open: true, severity: "success", message: "Email sent!" });
+            setmyEmail({ name: "", email: "", phone: "", message: "" });
+        }
+    };
+
+    http.open('POST', 'https://formspree.io/f/myzknyzb', true);
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    http.send(`name=${myEmail.name}&phone=${myEmail.phone}&email=${myEmail.email}&message=${myEmail.message}`);
+    */
+
+    
 }
--- contact.markdown
+++ contact.markdown
@@ -6,4 +6,60 @@ permalink: /contact/
 
 ## **Contact Me**
 ---
-In work
+
+<form class="row g-3 needs-validation" autoComplete="off" novalidate>
+<div class="parent">
+<div class="div1">
+    <div class="mb-3" style="box-sizing: border-box;">
+        <label for="form-name" class="form-label">Name:</label>
+        <input type="text" class="form-control" id="form-name" placeholder="" name="name" required>
+    </div>
+</div>
+<div class="div2">
+    <div class="mb-3" style="box-sizing: border-box;">
+        <label for="form-phone" class="form-label">Phone:</label>
+        <input type="tel" class="form-control" id="form-phone" placeholder="" name="phone" required>
+    </div>
+</div>
+<div class="div3">
+    <div class="mb-3" style="box-sizing: border-box;">
+        <label for="form-email" class="form-label">Email:</label>
+        <input type="email" class="form-control" id="form-email" placeholder="" name="email" required>
+    </div>
+</div>
+<div class="div4">
+    <div class="mb-3" style="box-sizing: border-box;">
+        <label for="form-message" class="form-label">Message</label>
+        <textarea class="form-control" id="form-message" rows="3" name="message" required></textarea>
+    </div>
+</div>
+<div class="div5">
+    <div class="mb-3" style="box-sizing: border-box;">
+        <button class="btn btn-primary" type="submit">Send</button>
+    </div>
+</div>
+</div>
+</form>
+
+<script>
+/*
+
+    (() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+        }, false)
+    })
+    })()*/
+</script>
